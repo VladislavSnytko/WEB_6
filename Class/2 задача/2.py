cd5769cc2bcd4730a8760f5dd58e796bf0b4284b
Vladislav Snytko <snitkov2006@gmail.com>
2023-02-07 20:12:37 +0300

    2 задача

--- Class/2 задача/2.py
+++ Class/2 задача/2.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 import requests
@@ -21,6 +22,13 @@ class MainWindow(QMainWindow):
 
         self.refresh_map()
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp and self.map_zoom < 17:
+            self.map_zoom += 1
+        if event.key() == Qt.Key_PageDown and self.map_zoom > 0:
+            self.map_zoom -= 1
+        self.refresh_map()
+
     def refresh_map(self):
         map_params = {
             "ll": ','.join(map(str, self.map_ll)),
@@ -28,13 +36,15 @@ class MainWindow(QMainWindow):
             'z': self.map_zoom
         }
 
-        response = requests.get('https://static-maps.yandex.ru/1.x/', params=map_params)
+        response = requests.get('https://static-maps.yandex.ru/1.x/',
+                                params=map_params)
         with open('tmp.png', mode='wb') as tmp:
             tmp.write(response.content)
 
         pixmap = QPixmap()
         pixmap.load('tmp.png')
         self.g_map.setPixmap(pixmap)
+        self.g_map.setScaledContents(True)
 
 
 app = QApplication(sys.argv)
